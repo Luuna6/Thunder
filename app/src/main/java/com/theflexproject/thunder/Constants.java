286420f8552769de59c10b0b24704db9f24e9609
Luna Bondy <128585436+Luuna6@users.noreply.github.com>
2023-04-27 22:31:44 +0100

    Update Constants.java

--- app/src/main/java/com/theflexproject/thunder/Constants.java
+++ app/src/main/java/com/theflexproject/thunder/Constants.java
@@ -8,13 +8,13 @@ public class Constants {
     public static final String TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
     public static final String TMDB_BACKDROP_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w1280";
     public static final String FANART_IMAGE_BASE_URL = "https://webservice.fanart.tv/v3/";
-    public static final String TMDB_API_KEY = "";
+    public static final String TMDB_API_KEY = "f2f9497153bf40ff458c26dfc271e015";
     public static final String SIMPLE_PROGRAM_DOWNLOAD_API = "https://geolocation.zindex.eu.org/generate.json?id=";
     public static final String CF_CACHE_TOKEN = "";
 
     public static String getFanartApiKey(){
         final String[] KEYS = {
-                "",
+                "7b172e6141cfab01e3403c56729e06e2",
                 "",
                 ""};
         Random random = new Random();
